38a66a10509fe1d8c4b4f11ba5136d53a3497aa8
SIYUAN LU <lsysx03@hotmail.com>
2022-12-24 21:00:28 -0800

    add authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -9,6 +9,7 @@ import { getRedirectResult } from "firebase/auth";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import "./authentication.styles.scss";
 
 const Authentication = () => {
   useEffect(() => {
@@ -30,8 +31,7 @@ const Authentication = () => {
   };
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
     </div>
